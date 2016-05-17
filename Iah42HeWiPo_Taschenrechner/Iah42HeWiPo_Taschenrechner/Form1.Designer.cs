1d45211594fffe74044e36d4729845ad64775f74
Bjarne <iah42herrmann@gso-koeln.de>
2016-05-18 00:26:51 +0200

    Klasse "Rechnung" hinzugefügt - Form bearbeitet

--- Iah42HeWiPo_Taschenrechner/Iah42HeWiPo_Taschenrechner/Form1.Designer.cs
+++ Iah42HeWiPo_Taschenrechner/Iah42HeWiPo_Taschenrechner/Form1.Designer.cs
@@ -30,6 +30,18 @@
         {
             this.AusgabeTextBox = new System.Windows.Forms.TextBox();
             this.ErgebnisButtonClick = new System.Windows.Forms.Button();
+            this.Zahl1ButtonClick = new System.Windows.Forms.Button();
+            this.EingabeGroupBox = new System.Windows.Forms.GroupBox();
+            this.Zahl2ButtonClick = new System.Windows.Forms.Button();
+            this.Zahl3ButtonClick = new System.Windows.Forms.Button();
+            this.Zahl4ButtonClick = new System.Windows.Forms.Button();
+            this.Zahl5ButtonClick = new System.Windows.Forms.Button();
+            this.Zahl6ButtonClick = new System.Windows.Forms.Button();
+            this.Zahl7ButtonClick = new System.Windows.Forms.Button();
+            this.Zahl8ButtonClick = new System.Windows.Forms.Button();
+            this.Zahl9ButtonClick = new System.Windows.Forms.Button();
+            this.Zahl0ButtonClick = new System.Windows.Forms.Button();
+            this.EingabeGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // AusgabeTextBox
@@ -37,29 +49,151 @@
             this.AusgabeTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.AusgabeTextBox.Location = new System.Drawing.Point(12, 12);
             this.AusgabeTextBox.Name = "AusgabeTextBox";
-            this.AusgabeTextBox.Size = new System.Drawing.Size(386, 44);
+            this.AusgabeTextBox.Size = new System.Drawing.Size(286, 44);
             this.AusgabeTextBox.TabIndex = 0;
             this.AusgabeTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // ErgebnisButtonClick
             // 
-            this.ErgebnisButtonClick.Location = new System.Drawing.Point(187, 191);
+            this.ErgebnisButtonClick.Location = new System.Drawing.Point(197, 357);
             this.ErgebnisButtonClick.Name = "ErgebnisButtonClick";
-            this.ErgebnisButtonClick.Size = new System.Drawing.Size(75, 23);
+            this.ErgebnisButtonClick.Size = new System.Drawing.Size(101, 39);
             this.ErgebnisButtonClick.TabIndex = 1;
             this.ErgebnisButtonClick.Text = "Ergebnis";
             this.ErgebnisButtonClick.UseVisualStyleBackColor = true;
             this.ErgebnisButtonClick.Click += new System.EventHandler(this.ErgebnisButtonClick_Click);
             // 
+            // Zahl1ButtonClick
+            // 
+            this.Zahl1ButtonClick.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Zahl1ButtonClick.Location = new System.Drawing.Point(6, 37);
+            this.Zahl1ButtonClick.Name = "Zahl1ButtonClick";
+            this.Zahl1ButtonClick.Size = new System.Drawing.Size(50, 50);
+            this.Zahl1ButtonClick.TabIndex = 2;
+            this.Zahl1ButtonClick.Text = "1";
+            this.Zahl1ButtonClick.UseVisualStyleBackColor = true;
+            // 
+            // EingabeGroupBox
+            // 
+            this.EingabeGroupBox.Controls.Add(this.Zahl0ButtonClick);
+            this.EingabeGroupBox.Controls.Add(this.Zahl9ButtonClick);
+            this.EingabeGroupBox.Controls.Add(this.Zahl5ButtonClick);
+            this.EingabeGroupBox.Controls.Add(this.Zahl8ButtonClick);
+            this.EingabeGroupBox.Controls.Add(this.Zahl4ButtonClick);
+            this.EingabeGroupBox.Controls.Add(this.Zahl7ButtonClick);
+            this.EingabeGroupBox.Controls.Add(this.Zahl3ButtonClick);
+            this.EingabeGroupBox.Controls.Add(this.Zahl6ButtonClick);
+            this.EingabeGroupBox.Controls.Add(this.Zahl2ButtonClick);
+            this.EingabeGroupBox.Controls.Add(this.Zahl1ButtonClick);
+            this.EingabeGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EingabeGroupBox.Location = new System.Drawing.Point(12, 76);
+            this.EingabeGroupBox.Name = "EingabeGroupBox";
+            this.EingabeGroupBox.Size = new System.Drawing.Size(286, 267);
+            this.EingabeGroupBox.TabIndex = 3;
+            this.EingabeGroupBox.TabStop = false;
+            this.EingabeGroupBox.Text = "Eingabe";
+            // 
+            // Zahl2ButtonClick
+            // 
+            this.Zahl2ButtonClick.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Zahl2ButtonClick.Location = new System.Drawing.Point(62, 37);
+            this.Zahl2ButtonClick.Name = "Zahl2ButtonClick";
+            this.Zahl2ButtonClick.Size = new System.Drawing.Size(50, 50);
+            this.Zahl2ButtonClick.TabIndex = 2;
+            this.Zahl2ButtonClick.Text = "2";
+            this.Zahl2ButtonClick.UseVisualStyleBackColor = true;
+            // 
+            // Zahl3ButtonClick
+            // 
+            this.Zahl3ButtonClick.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Zahl3ButtonClick.Location = new System.Drawing.Point(118, 37);
+            this.Zahl3ButtonClick.Name = "Zahl3ButtonClick";
+            this.Zahl3ButtonClick.Size = new System.Drawing.Size(50, 50);
+            this.Zahl3ButtonClick.TabIndex = 2;
+            this.Zahl3ButtonClick.Text = "3";
+            this.Zahl3ButtonClick.UseVisualStyleBackColor = true;
+            // 
+            // Zahl4ButtonClick
+            // 
+            this.Zahl4ButtonClick.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Zahl4ButtonClick.Location = new System.Drawing.Point(174, 37);
+            this.Zahl4ButtonClick.Name = "Zahl4ButtonClick";
+            this.Zahl4ButtonClick.Size = new System.Drawing.Size(50, 50);
+            this.Zahl4ButtonClick.TabIndex = 2;
+            this.Zahl4ButtonClick.Text = "4";
+            this.Zahl4ButtonClick.UseVisualStyleBackColor = true;
+            // 
+            // Zahl5ButtonClick
+            // 
+            this.Zahl5ButtonClick.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Zahl5ButtonClick.Location = new System.Drawing.Point(230, 37);
+            this.Zahl5ButtonClick.Name = "Zahl5ButtonClick";
+            this.Zahl5ButtonClick.Size = new System.Drawing.Size(50, 50);
+            this.Zahl5ButtonClick.TabIndex = 2;
+            this.Zahl5ButtonClick.Text = "5";
+            this.Zahl5ButtonClick.UseVisualStyleBackColor = true;
+            // 
+            // Zahl6ButtonClick
+            // 
+            this.Zahl6ButtonClick.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Zahl6ButtonClick.Location = new System.Drawing.Point(6, 93);
+            this.Zahl6ButtonClick.Name = "Zahl6ButtonClick";
+            this.Zahl6ButtonClick.Size = new System.Drawing.Size(50, 50);
+            this.Zahl6ButtonClick.TabIndex = 2;
+            this.Zahl6ButtonClick.Text = "6";
+            this.Zahl6ButtonClick.UseVisualStyleBackColor = true;
+            // 
+            // Zahl7ButtonClick
+            // 
+            this.Zahl7ButtonClick.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Zahl7ButtonClick.Location = new System.Drawing.Point(62, 93);
+            this.Zahl7ButtonClick.Name = "Zahl7ButtonClick";
+            this.Zahl7ButtonClick.Size = new System.Drawing.Size(50, 50);
+            this.Zahl7ButtonClick.TabIndex = 2;
+            this.Zahl7ButtonClick.Text = "7";
+            this.Zahl7ButtonClick.UseVisualStyleBackColor = true;
+            // 
+            // Zahl8ButtonClick
+            // 
+            this.Zahl8ButtonClick.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Zahl8ButtonClick.Location = new System.Drawing.Point(118, 93);
+            this.Zahl8ButtonClick.Name = "Zahl8ButtonClick";
+            this.Zahl8ButtonClick.Size = new System.Drawing.Size(50, 50);
+            this.Zahl8ButtonClick.TabIndex = 2;
+            this.Zahl8ButtonClick.Text = "8";
+            this.Zahl8ButtonClick.UseVisualStyleBackColor = true;
+            // 
+            // Zahl9ButtonClick
+            // 
+            this.Zahl9ButtonClick.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Zahl9ButtonClick.Location = new System.Drawing.Point(174, 93);
+            this.Zahl9ButtonClick.Name = "Zahl9ButtonClick";
+            this.Zahl9ButtonClick.Size = new System.Drawing.Size(50, 50);
+            this.Zahl9ButtonClick.TabIndex = 2;
+            this.Zahl9ButtonClick.Text = "9";
+            this.Zahl9ButtonClick.UseVisualStyleBackColor = true;
+            // 
+            // Zahl0ButtonClick
+            // 
+            this.Zahl0ButtonClick.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Zahl0ButtonClick.Location = new System.Drawing.Point(230, 93);
+            this.Zahl0ButtonClick.Name = "Zahl0ButtonClick";
+            this.Zahl0ButtonClick.Size = new System.Drawing.Size(50, 50);
+            this.Zahl0ButtonClick.TabIndex = 2;
+            this.Zahl0ButtonClick.Text = "0";
+            this.Zahl0ButtonClick.UseVisualStyleBackColor = true;
+            // 
             // RechnerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(426, 292);
+            this.ClientSize = new System.Drawing.Size(310, 408);
+            this.Controls.Add(this.EingabeGroupBox);
             this.Controls.Add(this.ErgebnisButtonClick);
             this.Controls.Add(this.AusgabeTextBox);
             this.Name = "RechnerForm";
             this.Text = "Rechner";
+            this.EingabeGroupBox.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -69,6 +203,17 @@
 
         private System.Windows.Forms.TextBox AusgabeTextBox;
         private System.Windows.Forms.Button ErgebnisButtonClick;
+        private System.Windows.Forms.Button Zahl1ButtonClick;
+        private System.Windows.Forms.GroupBox EingabeGroupBox;
+        private System.Windows.Forms.Button Zahl5ButtonClick;
+        private System.Windows.Forms.Button Zahl4ButtonClick;
+        private System.Windows.Forms.Button Zahl3ButtonClick;
+        private System.Windows.Forms.Button Zahl2ButtonClick;
+        private System.Windows.Forms.Button Zahl0ButtonClick;
+        private System.Windows.Forms.Button Zahl9ButtonClick;
+        private System.Windows.Forms.Button Zahl8ButtonClick;
+        private System.Windows.Forms.Button Zahl7ButtonClick;
+        private System.Windows.Forms.Button Zahl6ButtonClick;
     }
 }
 
---
+++ Iah42HeWiPo_Taschenrechner/Iah42HeWiPo_Taschenrechner/Rechnung.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iah42HeWiPo_Taschenrechner
+{
+    class Rechnung
+    {
+        private double eingabeZahl1;
+        private double eingabeZahl2;
+        private double ergebnis;
+
+        public double EingabeZahl1
+        {
+            set { eingabeZahl1 = value; }
+            get { return eingabeZahl1; }
+        }
+
+        public double EingabeZahl2
+        {
+            set { eingabeZahl2 = value; }
+            get { return eingabeZahl2; }
+        }
+
+        public double Ergebnis
+        {
+            set { ergebnis = value; }
+            get { return ergebnis; }
+        }
+
+
+        public void Addieren(double eingabeZahl1, double eingabeZahl2)
+        {
+            Ergebnis = eingabeZahl1 + eingabeZahl2;
+        }
+
+        public void Subtrahieren(double eingabeZahl1, double eingabeZahl2)
+        {
+            Ergebnis = eingabeZahl1 - eingabeZahl2;
+        }
+
+        public void Multiplizieren(double eingabeZahl1, double eingabeZahl2)
+        {
+            Ergebnis = eingabeZahl1 * eingabeZahl2;
+        }
+
+        public void Dividieren(double eingabeZahl1, double eingabeZahl2)
+        {
+            Ergebnis = eingabeZahl1 / eingabeZahl2;
+        }
+
+        public void Quadrieren(double eingabeZahl1, double eingabeZahl2)
+        {
+            Ergebnis = eingabeZahl1 * eingabeZahl1;
+        }
+
+        public void Wurzel(double eingabeZahl1, double eingabeZahl2)
+        {
+            Ergebnis = Math.Sqrt(eingabeZahl1);
+        }
+
+    }
+}
